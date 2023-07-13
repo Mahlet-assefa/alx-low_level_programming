3b6f8863932e0bd1e948d79f35e4ce43f8b2ab45
Mahlet-assefa <mahletassefa07@gmail.com>
2023-07-13 13:45:21 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main() {
+    printf("Size of char: %d byte(s)\n",sizeof(char));
+    printf("Size of int: %d byte(s)\n", sizeof(int));
+    printf("Size of long int: %d byte(s)\n", sizeof(long int));
+    printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+    printf("Size of float: %d byte(s)\n", sizeof(float));
+   
+
+    return 0;
+}
