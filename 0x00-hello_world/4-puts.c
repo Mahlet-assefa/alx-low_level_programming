b4511bc49cea23c5b89c547921db527426134d80
Mahlet-assefa <mahletassefa07@gmail.com>
2023-07-16 14:37:00 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - prints "Programming is like building a multilingual
-* puzzle, followed by a newline."
-* Return: Always 0.
-*/
+ * main - Entry point
+ *
+ * Description: Uses the function "puts" to write to stdout
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
