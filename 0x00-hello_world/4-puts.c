75a7abcb87b16e49b9cd0868d1b57d7162e6bc55
Mahlet-assefa <mahletassefa07@gmail.com>
2023-07-16 12:12:38 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
 * main - prints "Programming is like building a multilingual
-* puzzle, followed by a new line
+* puzzle, followed by a newline."
 * Return: Always 0.
 */
 int main(void)
 {
-puts("/"programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
 
