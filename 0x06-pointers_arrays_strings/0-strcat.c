766e6db77e44ad3afc59d4c7fceb52cd50689b53
Mahlet-assefa <mahletassefa07@gmail.com>
2023-07-27 10:27:51 -0700

    Done task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,30 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings to make a word
+ * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
+ *
  * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	int k;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	k = 0;
-	while (src[k] != '\0')
+	j = 0;
+	while (src[j] != '\0')
 	{
-		dest[i] = src[k];
+		dest[i] = src[j];
 		i++;
-		k++;
+		j++;
 	}
 
 	dest[i] = '\0';
 	return (dest);
 }
+
