b802c0d12bc7865dff29493ed67cef8322007512
Mahlet-assefa <mahletassefa07@gmail.com>
2023-08-29 11:34:02 -0700

    fix errors

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * free_listint2 - frees a linked list
  * @head: pointer to the listint_t list to be freed
@@ -6,13 +7,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
 	if (head == NULL)
 		return;
+
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
+
 	*head = NULL;
 }
